baddb5cf9c9895f3738c09dd5ed570360692fe3f
Pierre <py@poujade.org>
2014-02-24 15:57:57 -0500

    reverted to blank slate

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Treebook::Application.config.secret_key_base = 'b4cbb756a7d662f251571b6462134f236d93f4b5c69174347216a099fa16b509d0ff0c28dd7dd08e5e949f97594bf61e41edeaa70d184a28aa69f2ba72fa5a18'
+Treebook::Application.config.secret_key_base = '92561b5f2e6a57ba61f0e77930275c15792bd154f2448143a6816c184025392d6b38126916cf768bd81a7f8ebc12e2b8086745268528509955d4e59fec457573'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224001824) do
+ActiveRecord::Schema.define(version: 20140224205612) do
 
   create_table "statuses", force: true do |t|
     t.string   "name"
